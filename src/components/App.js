53987ecf6aa2f9889031b2742abb18a4fbf56660
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-02 00:42:59 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                <li key="relativeList1">Uncle & Aunty</li>
+                <li key="relativeList2">Grand Parents</li>
+                <li key="relativeList3">Cousin Rahul</li>
+                <li key="relativeList4">Sis</li>
+                <li key="relativeList5">Brother</li>
+               </ol>
     </div>
   )
 }
